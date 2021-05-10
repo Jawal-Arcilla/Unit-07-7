d547f934e93408a8a37f5beb03f7c45797ea3d7e
JawalArcilla <>
2021-05-10 13:32:32 +0000

    Attempt #1 on fixing errors

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ let answer = 0
 let counter = 0
 
 function calculate () {
- number1 = document.getElementById('first').value
- number2 = document.getElementById('second').value
- number1 = parseInt(number1)
- number2 = parseInt(number2)
+  number1 = document.getElementById('first').value
+  number2 = document.getElementById('second').value
+  number1 = parseInt(number1)
+  number2 = parseInt(number2)
 
- for (counter = 0; counter < number1; counter ++) 
+  for (counter = 0; counter < number1; counter ++)
  {
-   answer = answer + number2
- }
- document.getElementById('result').innerHTML = answer
+    answer = answer + number2
+  }
+  document.getElementById('result').innerHTML = answer
 }
